d94386ffc2c3ecdded5e7ae7400160a335e12d47
mesi <mesicharida202@gmail.com>
2024-05-22 20:51:06 +0900

    gtagの導入

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'nes.css/css/nes.min.css'
 import 'element-plus/dist/index.css'
+import VueGTag from 'vue-gtag'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -11,5 +12,8 @@ const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+app.use(VueGTag, {
+  config: { id: import.meta.env.VITE_GTAG_ID }
+})
 
 app.mount('#app')
