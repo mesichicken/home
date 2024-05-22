3cf7364d2ede01a974588db9123d633a523e784f
mesi <mesicharida202@gmail.com>
2024-05-22 19:50:43 +0900

    vercel.jsonの修正など

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +12,7 @@ const router = createRouter({
     {
       path: '/me',
       name: 'me',
-      component: ProfileView
+      component: () => import('../views/ProfileView.vue')
     },
   ]
 })
